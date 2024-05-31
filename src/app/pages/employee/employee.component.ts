e1717252507d4f3183948593a9bcc2dcf2114867
Dami-Otero <damiango.lucas@gmail.com>
2024-05-31 15:10:32 +0200

    Final form

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -13,45 +13,47 @@ import { IEmployee } from '../shared/models/Employee';
 })
 export class EmployeeComponent implements OnInit{
   isModelOpen = false;
-  employees: IEmployee[] = [];
-  employee!: IEmployee;
+  employees: IEmployee[] = []; //array that holds list of employees
+  employee!: IEmployee; //for one employees data
 constructor(private employeeService: EmployeeService, private toastService: ToastrService){}
 
+//basic function that initializes componrnt
 ngOnInit(): void{
-  this.getAllEmployee();
+  this.getAllEmployee(); //fetches employees once component initialized
+  console.log('ngOnInit') 
 }
 
-getAllEmployee(){
+getAllEmployee(){ //method to get employees
 
   this.employeeService.getAllEmployee().subscribe({
     next: (response) => {
-      this.employees = response.data;
+      if (response && Array.isArray(response))  //only passes the data when it is in an arrray to avoid undefined entries
+        this.employees = response; //assignes fetched data to this.employees
+      console.log('Fetched employees', this.employees);
     }
   })
 }
 
-deleteEmployee(id: string){
+deleteEmployee(id: string){ //method for deleting employees
 
   this.employeeService.deleteEmployee(id).subscribe({
     next: (response) => {
       this.toastService.success(response.message)
       this.getAllEmployee()
-
-
     }
   })
 }
 
-loadEmployee(data: IEmployee){
+loadEmployee(data: IEmployee){ //loads employee data onto the form
   this.employee = data;
   this.openModel();
 }
   openModel() {
-    this.isModelOpen = true;
+    this.isModelOpen = true; //true so the model is displayed
   }
 
   closeModel() {
-    this.isModelOpen = false;
+    this.isModelOpen = false; //hides model
     this.getAllEmployee()
   }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,31 +4,31 @@ import { ApiResponse, IEmployee } from '../pages/shared/models/Employee';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' //service provided at root level
 })
 export class EmployeeService {
 
-  api= "http://localhost:3000/employee";
+  api= "http://localhost:3000/employee"; 
 
   constructor(private http: HttpClient) { }
 
-  getAllEmployee(): Observable<ApiResponse<IEmployee[]>> {
+  getAllEmployee(): Observable<ApiResponse<IEmployee[]>> { //fetches employees and returns api response observable along with array
     return this.http.get<ApiResponse<IEmployee[]>>(`${this.api}`);
   }
 
-  getEmployee(id: string): Observable<ApiResponse<IEmployee>> {
+  getEmployee(id: string): Observable<ApiResponse<IEmployee>> { //fetches just one employee
     return this.http.get<ApiResponse<IEmployee>>(`${this.api}/${id}`);
   }
 
-  createEmployee(employee: IEmployee): Observable<ApiResponse<IEmployee>> {
+  createEmployee(employee: IEmployee): Observable<ApiResponse<IEmployee>> { //creates new employee
     return this.http.post<ApiResponse<IEmployee>>(`${this.api}`, employee);
   }
 
-  updateEmployee(id: string, employee: IEmployee): Observable<ApiResponse<IEmployee>> {
+  updateEmployee(id: string, employee: IEmployee): Observable<ApiResponse<IEmployee>> { //updates employee
     return this.http.put<ApiResponse<IEmployee>>(`${this.api}/${id}`, employee);
   }
   
-  deleteEmployee(id: string): Observable<ApiResponse<IEmployee>> {
+  deleteEmployee(id: string): Observable<ApiResponse<IEmployee>> { //deletes employee
     return this.http.delete<ApiResponse<IEmployee>>(`${this.api}/${id}`);
   }
 }
