b954bd3fe089ac93d80bdcb18cfb467176f4d118
Dami-Otero <damiango.lucas@gmail.com>
2024-06-03 10:52:14 +0200

    Comments

--- src/app/pages/employee-form/employee-form.component.ts
+++ src/app/pages/employee-form/employee-form.component.ts
@@ -5,19 +5,20 @@ import { IEmployee } from '../shared/models/Employee';
 import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-employee-form',
-  standalone: true,
+  standalone: true, //standalone component
   imports: [ReactiveFormsModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.scss',
 })
 export class EmployeeFormComponent implements OnChanges {
 
-  @Input() data: IEmployee | null=null;
-  @Output() onCloseModel = new EventEmitter();
-  employeeForm: FormGroup;
+  @Input() data: IEmployee | null=null; //input property to recive employee data
+  @Output() onCloseModel = new EventEmitter(); //notifies component about closure
+  employeeForm: FormGroup; //used to manage the form
 
   constructor(private fb: FormBuilder, private employeeService: EmployeeService, private toastService: ToastrService) {
-    this.employeeForm = this.fb.group({
+    this.employeeForm = this.fb.group({ 
+      //required validators to create the form group
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
       mobile: new FormControl('', [Validators.required]),
@@ -30,6 +31,7 @@ export class EmployeeFormComponent implements OnChanges {
   
   ngOnChanges(): void {
     if (this.data) {
+      //organizes data if not null
       this.employeeForm.patchValue({
         name: this.data?.name,
         email: this.data?.email,
@@ -39,13 +41,13 @@ export class EmployeeFormComponent implements OnChanges {
     })
   }
 }
-  onClose() {
+  onClose() { //closes form
     this.onCloseModel.emit(false);
   }
-  onSubmit(){
+  onSubmit(){ //for form submission
     if (this.employeeForm.valid) {
 
-      if (this.data){
+      if (this.data){ //updates the employee if the data exists
         this.employeeService.updateEmployee(this.data.id as string, this.employeeForm.value).subscribe({next:(response)=>
           {
             this.toastService.success(response.message);
@@ -55,7 +57,7 @@ export class EmployeeFormComponent implements OnChanges {
           })
 
       }
-      else {
+      else { //creates new emplpyee if data does not exist
       this.employeeService.createEmployee(this.employeeForm.value).subscribe({next:(response)=>
       {
         this.toastService.success(response.message);
